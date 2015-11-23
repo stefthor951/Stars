aedb0ca412b62d5a8f358795f15245e1b7585c1e
stefthor951 <stefthor951@ed.amdsb.ca>
2015-11-23 14:46:45 -0500

    Finished code

--- Stars/Form1.Designer.cs
+++ Stars/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(105, 119);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Stars!";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- Stars/Form1.cs
+++ Stars/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Stars
 {
@@ -17,10 +18,32 @@ namespace Stars
             InitializeComponent();
         }
 
-
-        public void StarField()
+        private void startButton_Click(object sender, EventArgs e)
         {
+            startButton.Dispose();
+            StarField(10000);
+        }
+        Random randNum = new Random();
+        public void StarField(int stars)
+        {
+            for (int counter = 0; counter <= stars; counter++)
+            {
+                Graphics g = this.CreateGraphics();
 
+
+                int xValue = randNum.Next(0, this.Width);
+                int yValue = randNum.Next(0, this.Height);
+                int redValue = randNum.Next(1, 256);
+                int greenValue = randNum.Next(1, 256);
+                int blueValue = randNum.Next(1, 256);
+                int starSize = randNum.Next(1, 100);
+                int starTransparency = randNum.Next(1, 151);
+
+                SolidBrush freakShow = new SolidBrush(Color.FromArgb(starTransparency, redValue, greenValue, blueValue));
+                g.FillEllipse(freakShow, xValue, yValue, starSize, starSize);
+
+                //Thread.Sleep(50);
+            }
         }
 
     }
